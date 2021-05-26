7aab74741a2e9fc4eae0efb07c919dc4989fefc0
Clay <78937867+cmanwaring@users.noreply.github.com>
2021-05-26 16:18:26 -0500

    Filtered metaData for sample input. Still working through.

--- charts.js
+++ charts.js
@@ -126,25 +126,28 @@ function buildCharts(sample) {
 
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metaData = data.metadata;
+    // Create filtered metaData
+    var filteredMetaData = metaData.filter(sampleObject => sampleObject.id == sample);
     // Create a variable that holds the first sample in the array.
-    var firstSample = filteredSample[0];
+    // var firstSample = filteredSample[0];
 
     // 2. Create a variable that holds the first sample in the metadata array.
-    var firstMetaData = metaData[0];
+    var firstFilteredMeta = filteredMetaData[0];
+    console.log(firstFilteredMeta);
 
     // Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    // var otu_ids = filteredSample[0].otu_ids;
+    // console.log(otu_ids);
+    // var otu_labels = filteredSample[0].otu_labels;
+    // console.log(otu_labels);
+    // var sample_values = filteredSample[0].sample_values;
+    // console.log(sample_values);
 
     // 3. Create a variable that holds the washing frequency.
-   
+    var wFreq = parseFloat(metaData.)
     // Create the yticks for the bar chart.
 
-    // Use Plotly to plot the bar data and layout.
-    Plotly.newPlot("bar", barData, barLayout);
-    
-    // Use Plotly to plot the bubble data and layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-   
+
     
     // 4. Create the trace for the gauge chart.
     var gaugeData = [
@@ -155,7 +158,12 @@ function buildCharts(sample) {
     var gaugeLayout = { 
      
     };
-
+    // Use Plotly to plot the bar data and layout.
+    Plotly.newPlot("bar", barData, barLayout);
+    
+    // Use Plotly to plot the bubble data and layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+   
     // 6. Use Plotly to plot the gauge data and layout.
     Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
