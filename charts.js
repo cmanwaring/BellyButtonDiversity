59cc491d97abeb917e54684f728ccf23eea412d8
Clay <78937867+cmanwaring@users.noreply.github.com>
2021-05-26 16:14:32 -0500

    Working through gauge chart.

--- charts.js
+++ charts.js
@@ -97,11 +97,7 @@ function buildCharts(sample) {
       xaxis: {title: "Sample Values"},
       yaxis: {title: "OTU ID"}
     };
-    // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar", barData, barLayout);
 
-    // Create Bubble chart.
-    var bubbleSize = sample_values 
     // 1. Create the trace for the bubble chart.
     var bubbleData = [{
       x: otu_ids,
@@ -123,7 +119,44 @@ function buildCharts(sample) {
       
     };
 
-    // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+    // Create gauge chart
+    // Create a variable that holds the samples array. 
+
+    // Create a variable that filters the samples for the object with the desired sample number.
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metaData = data.metadata;
+    // Create a variable that holds the first sample in the array.
+    var firstSample = filteredSample[0];
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var firstMetaData = metaData[0];
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+
+    // 3. Create a variable that holds the washing frequency.
+   
+    // Create the yticks for the bar chart.
+
+    // Use Plotly to plot the bar data and layout.
+    Plotly.newPlot("bar", barData, barLayout);
+    
+    // Use Plotly to plot the bubble data and layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+   
+    
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+     
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
